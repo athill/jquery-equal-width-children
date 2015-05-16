10d6a882665ed4c42d60ae344e140acdf69b27ee
Andy Hill <andy@andyhill.us>
2015-05-16 17:50:16 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# jquery-equal-width-children
+
+A rather simple plugin that tries to make the children of a container equal widths
+
+
+	<style>
+	.child {
+		display: inline-block;
+	}
+	</style>
+	...
+	<body>
+	<ul id="container">
+		<li class="child">child 1
+		<li class="child">child 2
+		<li class="child">child 3
+		<li class="child">child 4
+	</ul>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	<script src="../src/jquery.equal-width-children.js"></script>		
+	<script>
+		$(function() {
+			$("#container").equalWidthChildren({
+				propertyName: "a custom value"
+			});
+		});
+	</script>		
+	</body>
---
+++ src/jquery.equal-width-children.js
@@ -0,0 +1,41 @@
+
+;(function ( $, window, document, undefined ) {
+
+	"use strict";
+		// Create the defaults once
+		var pluginName = "equalWidthChildren",
+			defaults = {
+				fudge: 0
+			};
+
+		// The actual plugin constructor
+		function Plugin ( element, options ) {
+				this.element = element;
+				this.settings = $.extend( {}, defaults, options );
+				this._defaults = defaults;
+				this._name = pluginName;
+				this.init();
+		}
+
+		// Avoid Plugin.prototype conflicts
+		$.extend(Plugin.prototype, {
+				init: function () {
+						var $element = $(this.element);
+						console.log($element.width());
+						var $children = $element.children();
+						var width = Math.floor($element.width()/$children.length) - this.settings.fudge;
+						$children.css('width', width);
+				}
+		});
+
+		// A really lightweight plugin wrapper around the constructor,
+		// preventing against multiple instantiations
+		$.fn[ pluginName ] = function ( options ) {
+				return this.each(function() {
+						if ( !$.data( this, "plugin_" + pluginName ) ) {
+								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+						}
+				});
+		};
+
+})( jQuery, window, document );
